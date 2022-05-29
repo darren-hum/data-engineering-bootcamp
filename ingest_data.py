75db265721490776d5526ff743889a1a98e8553a
darren-hum <darren.hum@gmail.com>
2022-05-29 23:45:33 +0800

    stuck - unhashable type: set

---
+++ ingest_data.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+import argparse
+
+def main(params):
+    user = params.user
+    pw = params.pw
+    host = params.host
+    db = params.db
+    table = params.table
+    url = params.url
+    port = params.port
+
+
+    engine = create_engine(f'postgresql://{user}:{pw}@{host}:{port}/{db}')
+
+    #read data from url
+    df = pd.read_parquet(url)
+
+    #assign headers
+    df.head(n=0).to_sql(name={table}, con=engine, if_exists='replace')
+
+    #to sql with postgres con
+    df.to_sql(  
+        name=table, 
+        con=engine, 
+        if_exists='append', 
+        chunksize=250000
+        )
+
+####################
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='ingest parquet data to postgres')
+    
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--pw', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table', help='name of the table to write results to')
+    parser.add_argument('--url', help='url of the parquet file')
+    
+
+    args = parser.parse_args()
+
+    main(args)
+
+
+
+
+
+
--- notes.md
+++ notes.md
@@ -15,13 +15,60 @@ docker run -it \
     -e POSTGRES_DB="ny_taxi" \
     -v dtc_postgres_volume_local:/var/lib/postgresql/data \
     -p 5432:5432 \
-    postgres:13
+    --network=pg-network \
+    --name pg-database \
+    postgres:13 
 ```
 
-## PGCLI
+## Postgres
+#### PGCLI
 
 ```
 pgcli -h localhost -p 5432 -u root -d ny_taxi
+```
+
+#### PGADMIN
+```
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    --network=pg-network \
+    --name pgdamin \
+    dpage/pgadmin4
+
+```
+#### Create Network
+
+```
+docker network create <NAME>
+```
+
+#### Ingestion Script
+```
+URL="https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2022-01.parquet"
+python ingest_data.py \
+    --user=root \
+    --pw=root \
+    --host=localhost \
+    --port=5432 \
+    --db=ny_taxi \
+    --table=yellow_taxi_trips \
+    --url=${URL}
+```
+
+```
+docker build -t taxi_ingest:v001 .
+docker run -it \
+    --network=pg-network \
+    taxi_ingest:v001 \
+    --user=root \
+    --pw=root \
+    --host=localhost \
+    --port=5432 \
+    --db=ny_taxi \
+    --table=yellow_taxi_trips_docker \
+    --url=${URL}
 ```
 # Week 2
 # Week 3
